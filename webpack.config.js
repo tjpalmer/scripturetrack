abde560d8cf11691dc3a4d3b45cc870188ca94d4
Tom <tjpalmer@tjpalmer.com>
2017-09-10 21:22:46 -0700

    Placeholders

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// let prod = process.argv.indexOf('-p') >= 0;
+let webpack = require('webpack');
+
+module.exports = {
+  devServer: {
+    hot: false,
+    inline: false,
+  },
+  entry: {
+    app: './src/main.tsx',
+    // vendor: [
+    //   'matter-js',
+    //   'three',
+    // ],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$|\.tsx$/,
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+  output: {filename: "app.js"},
+  plugins: [
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor', filename: 'vendor.js',
+    // }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+};
+
+// if (prod) {
+//   module.exports.resolve.alias.three = 'three/build/three.min.js';
+// }
