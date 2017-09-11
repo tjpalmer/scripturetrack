63fd1222a801a4e855b13494598dc2554a57b174
Tom <tjpalmer@tjpalmer.com>
2017-09-10 21:27:12 -0700

    Being lazy and adding the compiled js

---
+++ app.js
@@ -0,0 +1,14 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+function init() {
+    console.log('Hi!');
+}
+window.addEventListener('load', init);
+
+
+/***/ })
+],[0]);
