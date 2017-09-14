0e8e226c480a79c828ef52e24b61c601a1cda7d3
Tom <tjpalmer@tjpalmer.com>
2017-09-14 06:05:22 -0700

    Start summaries

--- scripts/src/summarize.ts
+++ scripts/src/summarize.ts
@@ -1,9 +1,26 @@
 import {argv} from 'process';
-import {open, readdirSync} from 'fs';
+import {join} from 'path';
+import {open, readdirSync, readFileSync} from 'fs';
+import {usfmParse} from '../../src/index';
 
-function read() {
-  let names = readdirSync(argv[2]).filter(name => name.endsWith('.usfm'));
-  console.log(names);
+function summarizeText(dir: string) {
+  let names = readdirSync(dir).filter(name => name.endsWith('.usfm')).sort();
+  let items = names.map(key => {
+    let path = join(dir, key);
+    let content = readFileSync(path).toString();
+    usfmParse(content);
+    return {key, size: content.length};
+  });
+  return {items};
 }
 
-read();
+function summarizeTexts(baseDir: string) {
+  let textDirs = readdirSync(baseDir).sort();
+  let summary = {
+    items: textDirs.map(key => ({key, ...summarizeText(join(baseDir, key))})),
+  };
+  console.log(summary);
+  console.log(summary.items[0].items.slice(0, 3));
+}
+
+summarizeTexts(argv[2]);
---
+++ src/usfm.ts
@@ -0,0 +1,6 @@
+export function usfmParse(text: string) {
+  // TODO Actual parsing.
+  for (let line of text.split('\n')) {
+    line = line.trim();
+  }
+}
