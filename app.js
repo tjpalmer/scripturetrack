f231f67955ac1ff08f549f7df8154f60b00bfc22
Tom Palmer <tjpalmer@tjpalmer.com>
2017-09-16 16:03:48 -0700

    Compiled files

--- app.js
+++ app.js
@@ -5,18 +5,147 @@ webpackJsonp([0],{
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_dom__ = __webpack_require__(1);
+
+// CONCATENATED MODULE: ./src/usfm.ts
+function usfmParse(text, includeText) {
+    let doc = {};
+    let lines = [];
+    let size = 0;
+    for (let line of text.split('\n')) {
+        line = line.trim();
+        // TODO Extract start first.
+        if (line.startsWith('\\h')) {
+            doc.title = stripTag(line);
+        }
+        else if (line.startsWith('\\mt1') || line.startsWith('\\imt1')) {
+            doc.titleFull = stripTag(line);
+        }
+        else {
+            // Remove Strong's references.
+            line = line.replace(/\|[^\\]*/g, '');
+            // Remove footnotes.
+            line = line.replace(/\\f\b.*?\\f\*/g, '');
+            // Remove other tags.
+            line = line.replace(/\\\w+\*?/g, '');
+            lines.push(line);
+            size += line.length + 1;
+        }
+    }
+    doc.size = size;
+    if (includeText) {
+        doc.text = lines.join('\n') + '\n';
+    }
+    return doc;
+}
+function stripTag(line) {
+    return line.replace(/^\S+\s+/, '');
+}
+
+// EXTERNAL MODULE: ./node_modules/preact-compat/dist/preact-compat.es.js
+var preact_compat_es = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./src/view.tsx
+
+
+
+class view_AppView extends preact_compat_es["Component"] {
+    constructor(props) {
+        super(props);
+        let { library } = props;
+        let end = 0;
+        let begins = library.items.map(volume => volume.items.map(doc => {
+            let prev = end;
+            end += doc.size;
+            return prev;
+        }));
+        let charIndex = random() * end;
+        let volumeIndex = -1;
+        let volumeBegins = begins.reverse().find((docBegins, index) => {
+            volumeIndex = begins.length - index - 1;
+            return docBegins[0] <= charIndex;
+        });
+        let docIndex = -1;
+        let docBegin = volumeBegins.reverse().find((docBegin, index) => {
+            docIndex = volumeBegins.length - index - 1;
+            return docBegin <= charIndex;
+        });
+        console.log(charIndex, volumeIndex, docIndex, library.items[volumeIndex].items[docIndex]);
+        let volume = library.items[volumeIndex];
+        let path = [volume.key, volume.items[docIndex].key];
+        let offset = charIndex - docBegin;
+        fetch(['texts', ...path].join('/')).then(response => {
+            response.text().then(text => {
+                text = usfmParse(text, true).text;
+                console.log(offset, text.length);
+                this.setState({ text });
+            });
+        });
+        this.setState({ offset, path });
+    }
+    render() {
+        let { offset, text } = this.state;
+        return (preact_compat_es["createElement"]("div", null,
+            text && preact_compat_es["createElement"]("div", null, text.slice(offset, offset + 1000)),
+            preact_compat_es["createElement"](view_LibraryView, Object.assign({}, this.props.library))));
+    }
+}
+class view_DocView extends preact_compat_es["Component"] {
+    render() {
+        return (preact_compat_es["createElement"]("div", null,
+            this.props.title,
+            " - ",
+            this.props.size));
+    }
+}
+class view_LibraryView extends preact_compat_es["Component"] {
+    render() {
+        return (preact_compat_es["createElement"]("ul", null, this.props.items.map(volume => preact_compat_es["createElement"]("li", null,
+            preact_compat_es["createElement"](view_VolumeView, Object.assign({}, volume))))));
+    }
+}
+class view_VolumeView extends preact_compat_es["Component"] {
+    render() {
+        return (preact_compat_es["createElement"]("div", null,
+            this.props.title,
+            preact_compat_es["createElement"]("ul", null, this.props.items.map(doc => preact_compat_es["createElement"]("li", null,
+                preact_compat_es["createElement"](view_DocView, Object.assign({}, doc)))))));
+    }
+}
+function random() {
+    let ints = new Uint8Array(8);
+    crypto.getRandomValues(ints);
+    ints[7] = 0x3f;
+    ints[6] |= 0xf0;
+    return new DataView(ints.buffer).getFloat64(0, true) - 1;
+}
+
+// CONCATENATED MODULE: ./src/index.ts
+
+
+
+// CONCATENATED MODULE: ./src/main.tsx
 /// <reference path="../node_modules/@types/react/index.d.ts" />
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+
+
 
 
 function init() {
-    __WEBPACK_IMPORTED_MODULE_1_react_dom__["render"](__WEBPACK_IMPORTED_MODULE_0_react__["createElement"](Clock, null), 
-    // document.getElementById('root'),
-    document.body);
+    return __awaiter(this, void 0, void 0, function* () {
+        let library = yield (yield fetch('texts/texts.json')).json();
+        let app = { library, path: [] };
+        Object(preact_compat_es["render"])(preact_compat_es["createElement"](view_AppView, Object.assign({}, app)), document.getElementById('root'));
+    });
 }
 window.addEventListener('load', init);
-class Clock extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
+class main_Clock extends preact_compat_es["Component"] {
     constructor(props) {
         super(props);
         this.state = { date: new Date() };
@@ -31,9 +160,9 @@ class Clock extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
         this.setState({ date: new Date() });
     }
     render() {
-        return (__WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("div", null,
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("h1", null, "Hello, world!"),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("h2", null,
+        return (preact_compat_es["createElement"]("div", null,
+            preact_compat_es["createElement"]("h1", null, "Hello, world!"),
+            preact_compat_es["createElement"]("h2", null,
                 "It is ",
                 this.state.date.toLocaleTimeString(),
                 ".")));
