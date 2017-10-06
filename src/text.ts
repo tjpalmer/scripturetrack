315218a8da2b39c3b7bb40739bc7b8f946259e3f
Tom <tjpalmer@tjpalmer.com>
2017-10-06 07:15:56 -0700

    Fix indentations

--- src/text.ts
+++ src/text.ts
@@ -1,69 +1,68 @@
 export interface Chapter {
+
+  number?: number;
+
+  paragraphs: Array<Paragraph>;
+
+  size: number;
+
+}
+
+export interface Doc extends DocSelf {
+
+  name: string;
+
+}
+
+export interface DocSelf {
+  // DocSelf doesn't know its context.
+  // It gets the uglier name because usually I care about docs with names on
+  // them.
+
+  chapterSizes?: Array<number>;
+
+  chapters?: Array<Chapter>;
+
+  id: string;
+
+  size: number;
+
+  title: string;
+
+  // titleFull: string;
   
-    number?: number;
+}
   
-    paragraphs: Array<Paragraph>;
+export interface Paragraph {
+
+  size: number;
+
+  verses: Array<Verse>;
   
-    size: number;
+}
   
-  }
-  
-  export interface Doc extends DocSelf {
-  
-    name: string;
-  
-  }
-  
-  export interface DocSelf {
-    // DocSelf doesn't know its context.
-    // It gets the uglier name because usually I care about docs with names on
-    // them.
-  
-    chapterSizes?: Array<number>;
-  
-    chapters?: Array<Chapter>;
-  
-    id: string;
-  
-    size: number;
-  
-    title: string;
-  
-    // titleFull: string;
-    
-  }
-    
-  export interface Paragraph {
-  
-    size: number;
-  
-    verses: Array<Verse>;
-    
-  }
-    
-  export interface Volume {
-  
-    items: Array<Doc>;
-  
-    name: string;
-  
-    size?: number;
-  
-    title: string;
-  
-  }
-  
-  export interface Library {
-  
-    items: Array<Volume>;
-  
-  }
-  
-  export interface Verse {
-  
-    number?: number;
-  
-    text: string;
-  
-  }
-  
+export interface Volume {
+
+  items: Array<Doc>;
+
+  name: string;
+
+  size?: number;
+
+  title: string;
+
+}
+
+export interface Library {
+
+  items: Array<Volume>;
+
+}
+
+export interface Verse {
+
+  number?: number;
+
+  text: string;
+
+}
